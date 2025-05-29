c22c6d456dd50190dfd4047204effbbe4fd0c022
Núria Balaguer <nyxscream@gmail.com>
2025-05-29 15:26:48 +0100

    change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dist',
+  base: '/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     react(),
     tailwindcss(),
