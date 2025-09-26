b1a41de8c2bb93aefebcbbba069d2269a4c7c1c7
Núria Balaguer <nyxscream@gmail.com>
2025-09-26 11:01:54 +0100

    small change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ export function Hero() {
         <h1 className="font-abril-fatface text-plum 2xl:text-9xl lg:text-6xl font-bold mb-6">UX with a twist of code and AI.</h1>
         <p className="font-asap-condensed text-plum/70 2xl:text-5xl lg:text-2xl mb-6">Because knowing how things are built makes designing them smarter, faster, and more human.</p>
         <p className="font-asap-condensed text-plum/70 2xl:text-4xl lg:text-2xl mb-6">I’m <strong>Núria</strong> — nice to meet you!</p>
-        <div className="flex justify-self-center my-20">
+        <div className="flex justify-self-center mt-10">
           <Button variant="primary" href="#projects" className="mx-4">View projects</Button>
           <Button variant="secondary" onClick={() => downloadFile(`${import.meta.env.BASE_URL}cv.pdf`)} className="mx-4">Download CV</Button>
         </div>
